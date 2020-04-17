4788dbe3552fce27ac64c23bcf0126b8da771875
happyPeiLin <18398322316@163.com>
2020-04-17 23:32:51 +0800

    3-2

--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,31 @@ router.get('/string', async (ctx, next) => {
 })
 
 router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
-  }
-})
+    ctx.body = {
+      title: 'koa2 json'
+    }
+  })
+
+  router.get('/testAsync', async (ctx) => {
+    global.console.log('start', new Date().getTime())
+    const a = await new Promise((resolve, reject) => {
+      setTimeout(function () {
+        global.console.log('async a', new Date().getTime())
+        resolve('ab')
+      }, 1000);
+    })
+    const b = await 123
+    const c = await new Promise((resolve, reject) => {
+      setTimeout(function () {
+        global.console.log('async c', new Date().getTime())
+        resolve('abc')
+      }, 2000);
+    })
+    ctx.body = {
+      a,
+      b,
+      c
+    }
+  })
 
 module.exports = router
